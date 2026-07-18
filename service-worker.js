fee47ae1bb9a51207bc2085f40d8d07aac9d7e2e
sujay077 <sujaypaul077@gmail.com>
2026-07-18 12:11:43 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "coc-calculator-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});  
